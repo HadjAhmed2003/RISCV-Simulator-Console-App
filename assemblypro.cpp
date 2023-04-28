152652eaa3b5067744d184686069ec61464472e9
HadjAhmed <hadj_ahmed@aucegypt.edu>
2023-04-28 19:23:45 +0300

    started working on the function for executing the program and the one for executing a particular instruction.

--- assemblypro.cpp
+++ assemblypro.cpp
@@ -36,6 +36,7 @@ bitset<32> t4(0);
 bitset<32> t5(0); 
 bitset<32> t6(0); 
 int PC; 
+
 void ReadData(){
     string path; 
     cout << "Enter Data file path: ";
@@ -62,6 +63,7 @@ void ReadData(){
     } 
     fin.close();
 }
+
 void uploadProgram(){
     string path; 
     cout << "Enter your program file: "; 
@@ -69,12 +71,48 @@ void uploadProgram(){
     fstream fin;
     string line;
     fin.open(path, ios::in);
+    int inc = 0;
     while(std::getline(fin, line))
     {
         program.push_back(line); 
+        std::stringstream ss(line);
+        string temp;
+        vector<string> inputs; 
+        while(std::getline(ss, temp, ' ')){
+            inputs.push_back(temp);
+        }
+        if(inputs.size()==1){
+            memory_locations[inputs[0]] = to_string(PC+(inc*4)); //keeping the labels' locations. 
+        }
+        inc++; 
     }
     fin.close();
 }
+
+void executeProgram(){
+    string line = program[0];
+    int i=0;
+    while(line != "ecall" && line != "EBREAK" && line != "FENCE"){ 
+        std::stringstream ss(line);
+        string temp;
+        vector<string> inputs; 
+        while(std::getline(ss, temp, ' ')){
+            inputs.push_back(temp);
+        }
+        if(inputs.size()==1){ //in case we encounter a label we go to the next line; 
+            i++; 
+            line = program[i]; 
+            continue; 
+        }
+        executeInstruction(inputs); 
+    }
+}
+
+void executeInstruction(vector<string> inputs){
+    string instruction = inputs[0]; 
+    
+}
+
 void LUI(bitset<32>& rd, int imm){
     bitset<32> temp(imm); 
     for(int i=31; i>=12; i--){
@@ -84,6 +122,7 @@ void LUI(bitset<32>& rd, int imm){
         rd[i]=0; 
     }
 }
+
 void AUIPC(bitset<32>& rd, int PC, int imm){
     int sum = PC+imm; 
     bitset<32> temp(sum); 
@@ -94,33 +133,40 @@ void AUIPC(bitset<32>& rd, int PC, int imm){
         rd[i]=0; 
     }
 }
+
 void AND(bitset<32>& rd, const bitset<32>& rs1, const bitset<32>& rs2){
     rd = rs1 & rs2; 
 }
+
 void OR(bitset<32>& rd, const bitset<32>& rs1, const bitset<32>& rs2){
     rd = rs1 | rs2; 
 }
+
 void XOR(bitset<32>& rd, const bitset<32>& rs1, const bitset<32>& rs2){
     rd = rs1 ^ rs2; 
 }
+
 void ANDI(bitset<32>& rd, const bitset<32>& rs1, int imm){
     bitset<32>temp(imm); 
     for(int i=0; i<32; i++){
         rd[i]=rs1[i]&temp[i];
     }
 }
+
 void ORI(bitset<32>& rd, const bitset<32>& rs1, int imm){
     bitset<32>temp(imm); 
     for(int i=0; i<32; i++){
         rd[i]=rs1[i]|temp[i];
     }
 }
+
 void XORI(bitset<32>& rd, const bitset<32>& rs1, int imm){
     bitset<32>temp(imm); 
     for(int i=0; i<32; i++){
         rd[i]=rs1[i]^temp[i];
     }
 }
+
 void ADD(bitset<32>& rd, const bitset<32>& rs1, const bitset<32>& rs2){
     int temp1 = (int)(rs1.to_ulong());
     int temp2 = (int)(rs2.to_ulong());
@@ -130,6 +176,7 @@ void ADD(bitset<32>& rd, const bitset<32>& rs1, const bitset<32>& rs2){
         rd[i]=temp[i];
     }
 }
+
 void SUB(bitset<32>& rd, const bitset<32>& rs1, const bitset<32>& rs2){
     int temp1 = (int)(rs1.to_ulong());
     int temp2 = (int)(rs2.to_ulong());
@@ -139,6 +186,7 @@ void SUB(bitset<32>& rd, const bitset<32>& rs1, const bitset<32>& rs2){
         rd[i]=temp[i];
     }
 }
+
 void ADDI(bitset<32>& rd, const bitset<32>& rs1, int imm){
     int temp = (int)(rs1.to_ulong());
     int sum = temp+imm; 
